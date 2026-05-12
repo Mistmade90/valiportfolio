39a50032977e17911fd5a674ad6521d33446fce9
Mistmade90 <mistmade90@gmail.com>
2026-05-12 12:04:23 +0200

    Chnge

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -322,7 +322,7 @@ export default function Portfolio() {
               Hi, I'm{" "}
 
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-400 via-pink-400 to-violet-500">
-                Valithar
+                Mistmade
               </span>
             </motion.h1>
 
